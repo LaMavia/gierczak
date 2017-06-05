f4f284c5049b45e8fcbc90e098e7788c1202dbd5
root <root@localhost.localdomain>
2017-06-05 18:42:59 +0200

    shopv0.2 Added

--- game.php
+++ game.php
@@ -7,12 +7,13 @@
         <title>Gierczak</title>
 		<link href="css-less_files/normalize.css" rel="stylesheet">
         <link href="css-less_files/style3.css" rel="stylesheet">
+		<link href="css/fontello.css" rel="stylesheet">
     </head>
     <body>
 <?php
 echo "Witaj ".$_SESSION["username"];
 ?>
-        <form class="box__main" action="sprawdz.php" method="post">
+        <div class="box__main">
 			<div class="menu__box">
 				<div class="usr_box">
 					<div class="save"><a href="save.php">Save</a></div>
@@ -28,12 +29,12 @@ echo "Witaj ".$_SESSION["username"];
 				<div class="hint">Hint</div>
 				<div class="shop">
 					<div class="shop__box">
-						<div>Lorem ipsum</div>
-						<div>dolor sit</div>
-						<div>amet</div>
+						<div class="shop__item" data-item="range">Range: 1-90</div>
+						<div class="shop__item" data-item="pps">Points/sec: 0</div>
 					</div>
 				</div>
-        </form>
+				<div class="dev__build">DevBuild</div>
+        </div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="js_files/game.js"></script>
     </body>
