25c022ea4b27c80d3a3ab871c907627a467dc852
Dominik <szymanowskidominik@gmail.com>
2017-06-17 08:25:04 +0200

    save
    save

--- save.php
+++ save.php
@@ -8,17 +8,22 @@
 	require_once "dbconnect.php";
 	$connect= @new mysqli($host, $db_user, $db_pass, $db_name);
 	if($connect->connect_errno != 0){
-			$json = file_get_contents("json_files/scores.json");
-			$obj = json_decode($json, true);
-			$points = $_SESSION['scores'];
-			$pps = $_SESSION[0]["upgrades"][0]["pps"];
-			$id = $_SESSION["nick"];
-			$sql = $connect->query("UPDATE daneuzytkownikow SET POINTS = '$points' WHERE ID = '$id'");
-			$sqlq = $connect->query("SELECT POINTS FROM daneuzytkownikow WHERE ID = '$id'");
-			$row = $sqlq->fetch_assoc();
-			$_SESSION["points"] = $row['POINTS'];
-			header('location: game.php');
-			$sqlq->free();
-			$connect->close();
+		die("There was an error");
+	}
+	else
+	{
+		$post = $_POST["userID"];
+		$json = file_get_contents("json_files/scores.json");
+		$obj = json_decode($json, true);
+		$points = $_SESSION['points'];
+		$pps = $obj[0]["upgrades"][0]["pps"];
+		$id = $_SESSION["id"];
+		$sql = $connect->query("UPDATE daneuzytkownikow SET POINTS = '$points' WHERE ID = '$id'");
+		$sqlq = $connect->query("SELECT POINTS FROM daneuzytkownikow WHERE ID = '$id'");
+		$row = $sqlq->fetch_assoc();
+		$_SESSION["points"] = $row['POINTS'];
+		header('location: game.php');
+		$sqlq->free();
+		$connect->close();
 	}
 ?>
